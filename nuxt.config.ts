6af366c0a47e9ba9f6446413aeb6d5a09c986913
EfimovSergeiV <efimovsergeiv@gmail.com>
2025-08-08 14:30:18 +0300

    Add:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
+  devtools: { enabled: true },
   modules: [
-    'nuxt-electron',
     '@nuxtjs/tailwindcss',
-  ],
-  electron: {
-    build: [
-      {
-        // Main-Process entry file of the Electron App.
-        entry: 'electron/main.ts',
-      },
-      {
-        entry: 'electron/preload.ts',
-        onstart(args) {
-          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete, 
-          // instead of restarting the entire Electron App.
-          args.reload()
+    ['nuxt-electron', {
+      build: [
+        {
+          // Main-Process entry file of the Electron App.
+          entry: 'electron/main.ts',
         },
-      },
-    ],
-    // Ployfill the Electron and Node.js API for Renderer process.
-    // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
-    // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-    renderer: {},
-  },
+        {
+          entry: 'electron/preload.ts',
+          onstart(args: any) {
+            // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete, 
+            // instead of restarting the entire Electron App.
+            args.reload()
+          },
+        },
+      ],
+      // Ployfill the Electron and Node.js API for Renderer process.
+      // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
+      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+      renderer: {},
+    }],
+  ],
   ssr: false, // #43
 })
